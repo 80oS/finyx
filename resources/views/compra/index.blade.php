11580dd52cbf0b7b4107797b4014aee49b89ecdf
fer-com <fernanda05r@gmail.com>
2026-09-21 21:59:32 -0500

    Se agregaron colores en el crud de compras

--- resources/views/compra/index.blade.php
+++ resources/views/compra/index.blade.php
@@ -16,34 +16,39 @@
         </div>
         <div class="flex items-end justify-end">
             <a href="{{ route('compra.create') }}" 
-            class="bg-blue-700 hover:bg-blue-900 text-white rounded-sm px-5 py-1 text-right transition-all w-20"
+              class="bg-verde-salvia hover:bg-verde-salvia text-blanco-calido 
+                rounded-sm px-5 py-1 text-right transition-all w-20"
             >crear</a>
         </div>
     </div>
 
-    <div class="overflow-x-auto rounded-md">
-        <table class="w-full text-left border-collapse">
-            <thead class="bg-gray-400 text-gray-800 uppercase text-xs text-center sticky">
+     <div class="overflow-x-auto rounded-md">
+        <table class="w-full text-left border-collapse border border-linea-exterior-tabla">
+            <thead class="bg-lino-natural text-gris-marron-calido capitalize text-sm 
+            text-center sticky border-b border-linea-thead">
                 <tr>
-                    <th class="px-6 py-3 font-medium border border-gray-800" scope="col">Id</th>
-                    <th class="px-6 py-3 font-medium border border-gray-800" scope="col">Proveedor</th>
-                    <th class="px-6 py-3 font-medium border border-gray-800" scope="col">Metodo pago</th>
-                    <th class="px-6 py-3 font-medium border border-gray-800" scope="col">Total</th>
-                    <th class="px-6 py-3 font-medium border border-gray-800" scope="col">Acciones</th>
+                    <th class="px-6 py-3 font-medium">Id</th>
+                    <th class="px-6 py-3 font-medium">Proveedor</th>
+                    <th class="px-6 py-3 font-medium">Metodo pago</th>
+                    <th class="px-6 py-3 font-medium">Total</th>
+                    <th class="px-6 py-3 font-medium">Acciones</th>
                     
                 </tr>
             </thead>
             <tbody class="divide-y divide-gray-600 text-sm text-gray-900">
                 @foreach ($compras as $compra)
-                    <tr class="bg-gray-300 hover:bg-gray-400 transition-all">
-                        <td class="p-3 border border-gray-800">{{ $compra->id }}</td>
-                        <td class="px-6 py-4 border border-gray-800">{{ $compra->proveedor->nombre }}</td>
-                        <td class="px-6 py-4 border border-gray-800">{{ $compra->metodo_pago}}</td>
-                        <td class="px-6 py-4 border border-gray-800">{{ $compra->total }}</td>
-                        <td class="px-6 py-4 border border-gray-800 text-center">
+                    <tr class="bg-blanco-calido hover:bg-crema-clarisimo transition-all border 
+                    border-linea-tr-tbody text-center text-xs">
+                        <td class="p-3">{{ $compra->id }}</td>
+                        <td class="p-3">{{ $compra->proveedor->nombre }}</td>
+                        <td class="p-3">{{ $compra->metodo_pago}}</td>
+                        <td class="p-3">{{ $compra->total }}</td>
+                        <td class="p-3">
                             <a href="{{ route('compra.edit', $compra->id) }}"
-                                class="bg-sky-500 hover:bg-sky-700 text-white rounded-sm px-5 py-1 transition-all">
-                                Editar</a>
+                               class="text-azul-grisaceo hover:text-azul-grisaceo-oscuro 
+                                text-base px-5 py-1 transition-all">
+                                <i class="fa-regular fa-pen-to-square"></i>
+                            </a>
                         </td>
                         
                     </tr>
